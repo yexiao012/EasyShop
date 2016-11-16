fc4aff8cda96de1ff740d2fb2475d77400e44f86
wxcican <wxcican@qq.com>
2016-11-16 17:18:35 +0800

    项目搭建2

---
+++ app/src/main/java/com/fuicuiedu/idedemo/easyshop/main/MainActivity.java
@@ -0,0 +1,160 @@
+package com.fuicuiedu.idedemo.easyshop.main;
+
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fuicuiedu.idedemo.easyshop.R;
+import com.fuicuiedu.idedemo.easyshop.commons.ActivityUtils;
+import com.fuicuiedu.idedemo.easyshop.main.me.MeFragment;
+import com.fuicuiedu.idedemo.easyshop.main.shop.ShopFragment;
+
+import java.util.List;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Unbinder unbinder;
+
+    @BindViews({R.id.tv_shop,R.id.tv_message,R.id.tv_mail_list,R.id.tv_me})
+    TextView[] textViews;
+
+    @BindView(R.id.main_toobar)
+    Toolbar toolbar;
+    @BindView(R.id.main_title)
+    TextView tv_title;
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+    //点击2次返回，退出程序
+    private boolean isExit = false;
+    private ActivityUtils activityUtils;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+
+        activityUtils = new ActivityUtils(this);
+        //设置一下actionbar
+        setSupportActionBar(toolbar);
+        //设置一下ActionBae标题为空，否则默认显示应用名
+        getSupportActionBar().setTitle("");
+
+        init();
+    }
+
+    //进入页面数据初始化，默认显示为市场页面
+    private void init(){
+        //刚进来默认选择市场
+        textViews[0].setSelected(true);
+        //TODO 用户是否登录，从而选择不同的适配器
+        viewPager.setAdapter(unLoginAdapter);
+        viewPager.setCurrentItem(0);
+
+        //viewpager添加滑动事件
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //textView选择处理
+                for (TextView textView : textViews){
+                    textView.setSelected(false);
+                }
+                //更改title，设置选择效果
+                tv_title.setText(textViews[position].getText());
+                textViews[position].setSelected(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+
+    private FragmentStatePagerAdapter unLoginAdapter = new
+            FragmentStatePagerAdapter(getSupportFragmentManager()) {
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                //市场
+                case 0:
+                    return new ShopFragment();
+                //消息
+                case 1:
+                    return new UnLoginFragment();
+                //通讯录
+                case 2:
+                    return new UnLoginFragment();
+                //我的
+                case 3:
+                    return new MeFragment();
+            }
+            return null;
+        }
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    };
+
+
+    //textview点击事件
+    @OnClick({R.id.tv_shop,R.id.tv_message,R.id.tv_mail_list,R.id.tv_me})
+    public void onClick(TextView view){
+        for(int i = 0;i<textViews.length;i++){
+            textViews[i].setSelected(false);
+            textViews[i].setTag(i);
+        }
+        //设置选择效果
+        view.setSelected(true);
+        //参数false代表瞬间切换，而不是平滑过渡
+        viewPager.setCurrentItem((Integer) view.getTag(),false);
+        //设置一下toolbar的title
+        tv_title.setText(textViews[(Integer) view.getTag()].getText());
+    }
+
+    //点击2次返回，退出程序
+    @Override
+    public void onBackPressed() {
+        if (!isExit){
+            isExit = true;
+            activityUtils.showToast("再摁一次退出程序");
+            //两秒内再次点击返回则退出
+            viewPager.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    isExit = false;
+                }
+            },2000);
+        }else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+}
---
+++ app/src/main/java/com/fuicuiedu/idedemo/easyshop/main/shop/ShopFragment.java
@@ -0,0 +1,29 @@
+package com.fuicuiedu.idedemo.easyshop.main.shop;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fuicuiedu.idedemo.easyshop.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShopFragment extends Fragment {
+
+
+    public ShopFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_shop, container, false);
+    }
+
+}
